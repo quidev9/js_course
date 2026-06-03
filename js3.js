0154ed15b383638feefe6a7028762f203d14c791
quidev9 <iwasrestored@gmail.com>
2026-06-03 15:49:11 +0300

    Типы данных в JavaScript. Проверка типов (typeof), преобразование типов. Интерполяция строк.

---
+++ js3.js
@@ -0,0 +1,22 @@
+console.log(100 / 0)
+
+console.log(99000000000000091n + 1n)
+
+console.log(1_10)
+
+
+console.log(
+    typeof object,
+    typeof null,
+    typeof [1, 2, 3]
+)
+
+num = 111
+str = '222'
+
+console.log(num + Number(str))
+
+console.log(String(num) + Number(str))
+
+console.log(num + Boolean(str))
+
